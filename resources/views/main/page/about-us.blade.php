a1194487979637ac971e57dd1caf8f568dbf42b4
Adam Surya Des <desadamsury93@gmail.com>
2022-04-11 14:25:46 +0700

    Update about-us.blade.php

--- resources/views/main/page/about-us.blade.php
+++ resources/views/main/page/about-us.blade.php
@@ -20,7 +20,7 @@
         color: rgb(19 169 229) !important;
     }
     nav#header a:hover{
-        color: white !important;
+        color: rgb(0 138 60) !important;
     }
     .title-section{
         color: rgb(19 169 229);
