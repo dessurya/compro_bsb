adcfb7f0310cddb0d9637954a77161e85c4f682b
Adam Surya Des <desadamsury93@gmail.com>
2022-05-10 11:51:35 +0700

    .

--- app/Http/Controllers/Cms/PublicConfigController.php
+++ app/Http/Controllers/Cms/PublicConfigController.php
@@ -10,9 +10,8 @@ class PublicConfigController extends Controller
     public function index()
     {
         $arrConf = json_decode(file_get_contents('config_json/public.json'),true);
-        // File::delete(storage_path('config_json/public.json'));
-        // $arrConf['asdasdsd'] = ['asdas','asdadasdasdas'];
-        // File::put(public_path('config_json/public_asdasd.json'),json_encode($arrConf));
+        $arrConf['asd'] = ['asd' => 'asd'];
+        file_put_contents('config_json/public_asd.json', json_encode($arrConf));
         return $arrConf;
     }
 }
