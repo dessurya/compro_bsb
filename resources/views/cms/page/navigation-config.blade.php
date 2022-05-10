47e9fff1daede0447896c6c3b69f19aaaca252d2
Adam Surya Des <desadamsury93@gmail.com>
2022-05-10 09:22:07 +0700

    .

--- resources/views/cms/page/navigation-config.blade.php
+++ resources/views/cms/page/navigation-config.blade.php
@@ -173,7 +173,7 @@ Navigasi Config
         $('#'+identity+' table tbody').html('')
         $.each(data, function(idx,row){
             let encode_data = btoa(JSON.stringify(row))
-            let render_row = '<tr id="row_data_'+row.id+'" ondbclick="selectedRowData(\'row_data_'+row.id+'\')">'
+            let render_row = '<tr id="row_data_'+row.id+'" ondblclick="selectedRowData(\'row_data_'+row.id+'\')">'
             render_row += '<td>'+row.identity+'</td>'
             render_row += '<td>'+row.name+'</td>'
             render_row += '<td>'+row.meta_title+'</td>'
@@ -192,48 +192,50 @@ Navigasi Config
 
     openData = (encode_data) => {
         let decode_data = JSON.parse(atob(encode_data))
+        console.log(decode_data)
         $('#ConfigPageShow').fadeIn("slow")
-        $('#ConfigPageShow .card-body [name=identity]').val(decode_data.identity)
-        $('#ConfigPageShow .card-body [name=meta_author]').val(decode_data.meta_author)
+        $('#ConfigPageShow [name=id]').val(decode_data.id)
+        $('#ConfigPageShow [name=identity]').val(decode_data.identity)
+        $('#ConfigPageShow [name=meta_author]').val(decode_data.meta_author)
 
         let name = JSON.parse(decode_data.name)
-        $('#ConfigPageShow .card-body [name=name_id]').val(decode_data.name.id)
-        $('#ConfigPageShow .card-body [name=name_en]').val(decode_data.name.en)
+        $('#ConfigPageShow [name=name_id]').val(decode_data.name.id)
+        $('#ConfigPageShow [name=name_en]').val(decode_data.name.en)
 
         if (decode_data.meta_description != null && decode_data.meta_description != '') {
             let meta_description = JSON.parse(decode_data.meta_description)
-            $('#ConfigPageShow .card-body [name=meta_description_id]').val(decode_data.meta_description.id)
-            $('#ConfigPageShow .card-body [name=meta_description_en]').val(decode_data.meta_description.en)
+            $('#ConfigPageShow [name=meta_description_id]').val(decode_data.meta_description.id)
+            $('#ConfigPageShow [name=meta_description_en]').val(decode_data.meta_description.en)
         }
         if (decode_data.meta_keywords != null && decode_data.meta_keywords != '') {
             let meta_keywords = JSON.parse(decode_data.meta_keywords)
-            $('#ConfigPageShow .card-body [name=meta_keywords_id]').val(decode_data.meta_keywords.id)
-            $('#ConfigPageShow .card-body [name=meta_keywords_en]').val(decode_data.meta_keywords.en)
+            $('#ConfigPageShow [name=meta_keywords_id]').val(decode_data.meta_keywords.id)
+            $('#ConfigPageShow [name=meta_keywords_en]').val(decode_data.meta_keywords.en)
         }
         if (decode_data.meta_title != null && decode_data.meta_title != '') {
             let meta_title = JSON.parse(decode_data.meta_title)
-            $('#ConfigPageShow .card-body [name=meta_title_id]').val(decode_data.meta_title.id)
-            $('#ConfigPageShow .card-body [name=meta_title_en]').val(decode_data.meta_title.en)
+            $('#ConfigPageShow [name=meta_title_id]').val(decode_data.meta_title.id)
+            $('#ConfigPageShow [name=meta_title_en]').val(decode_data.meta_title.en)
         }
     }
 
     submitFormConfigPage = async () => {
         let param = {}
-        param.id = $('#ConfigPageShow .card-body [name=id]').val()
-        param.identity = $('#ConfigPageShow .card-body [name=identity]').val()
-        param.meta_author = $('#ConfigPageShow .card-body [name=meta_author]').val()
+        param.id = $('#ConfigPageShow [name=id]').val()
+        param.identity = $('#ConfigPageShow [name=identity]').val()
+        param.meta_author = $('#ConfigPageShow [name=meta_author]').val()
         param.name = {}
-        param.name.id = $('#ConfigPageShow .card-body [name=name_id]').val()
-        param.name.en = $('#ConfigPageShow .card-body [name=name_en]').val()
+        param.name.id = $('#ConfigPageShow [name=name_id]').val()
+        param.name.en = $('#ConfigPageShow [name=name_en]').val()
         param.meta_description = {}
-        param.meta_description.id = $('#ConfigPageShow .card-body [name=meta_description_id]').val()
-        param.meta_description.en = $('#ConfigPageShow .card-body [name=meta_description_en]').val()
+        param.meta_description.id = $('#ConfigPageShow [name=meta_description_id]').val()
+        param.meta_description.en = $('#ConfigPageShow [name=meta_description_en]').val()
         param.meta_keywords = {}
-        param.meta_keywords.id = $('#ConfigPageShow .card-body [name=meta_keywords_id]').val()
-        param.meta_keywords.en = $('#ConfigPageShow .card-body [name=meta_keywords_en]').val()
+        param.meta_keywords.id = $('#ConfigPageShow [name=meta_keywords_id]').val()
+        param.meta_keywords.en = $('#ConfigPageShow [name=meta_keywords_en]').val()
         param.meta_title = {}
-        param.meta_title.id = $('#ConfigPageShow .card-body [name=meta_title_id]').val()
-        param.meta_title.en = $('#ConfigPageShow .card-body [name=meta_title_en]').val()
+        param.meta_title.id = $('#ConfigPageShow [name=meta_title_id]').val()
+        param.meta_title.en = $('#ConfigPageShow [name=meta_title_en]').val()
         await httpRequest('{{ route("cms.navigation-config.store") }}','post',param).then(function(result){ return result })
         await refreshTable()
         showPNotify('Info','Success')
