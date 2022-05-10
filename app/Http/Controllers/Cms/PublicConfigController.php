3e6eb601d794fd96474117b8e0cb4e8df8fe9068
Adam Surya Des <desadamsury93@gmail.com>
2022-05-10 11:15:07 +0700

    .

--- app/Http/Controllers/Cms/PublicConfigController.php
+++ app/Http/Controllers/Cms/PublicConfigController.php
@@ -5,13 +5,13 @@ namespace App\Http\Controllers\Cms;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use File;
+use Illuminate\Support\Facades\Storage;
 
 class PublicConfigController extends Controller
 {
     public function index()
     {
-        $files = File::get('\storage\config_json\public.json');
+        $files = Storage::get('config_json\public.json');
         return $files;
     }
 }
