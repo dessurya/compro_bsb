ca844145a654e0d31c09b3b9c5d5fdf6f732eb4d
Adam Surya Des <desadamsury93@gmail.com>
2022-05-10 11:07:25 +0700

    .

--- app/Http/Controllers/Cms/PublicConfigController.php
+++ app/Http/Controllers/Cms/PublicConfigController.php
@@ -11,7 +11,7 @@ class PublicConfigController extends Controller
 {
     public function index()
     {
-        $files = Storage::get(asset('config_json/public.json'));
+        $files = Storage::get('public/config_json/public.json');
         return $files;
     }
 }
