56afd88fb6d9b46a4a998a761dde77c5bacb0e33
Adam Surya Des <desadamsury93@gmail.com>
2022-05-12 10:56:09 +0700

    .

--- app/Http/Controllers/Cms/PageConfigAboutUsController.php
+++ app/Http/Controllers/Cms/PageConfigAboutUsController.php
@@ -13,27 +13,89 @@ class PageConfigAboutUsController extends Controller
     {
         $file = $this->getFileDir;
         $arrConf = [
-            'banner' => [
-                'max_item' => 3,
-            ],
-            'news_info' => [
-                'max_item' => 3,
-            ],
-            'quotes' => [
-                'line_1' => 'Kami tidak hanya mengejar keuntungan',
-                'line_2' => 'Tetapi reputasi yang layak dibanggakan bangsa dan negara',
-                'imgs_1' => null,
-                'imgs_2' => null,
-                'imgs_3' => null,
-                'imgs_4' => null,
-            ],
-            'our_client' => [
-                'background' => null,
+            'intruduction' => [
+                'id' => [
+                    'title' => 'Sejarah',
+                    'content' => '<p>In 1986, the company obtained a pearl cultivation business license from the Director-General of Fisheries No.IK.120/03.5757/86, dated September 29, 1986, and a Permanent Approval Letter of PMDN No.269/I/PMDN/1986 dated November 24, 1986.</p><p>In 1992, the company obtained a Permanent Business License from the Chairman of BKPM No.30/T/PERTANIAN/1992 dated January 25, 1992, wherefrom December 1988, the company was producing commercially for the area in Sape Bay, Bima Regency, NTB.</p><p>In 2020, PT. Bima Sakti Bahari received recommendations for the extension of its business in trade and fisheries business for the waters of Sape bay and Tengge waters and waters of Radu, Bima Regency, NTB Province</p><p>In 2021, PT. Bima Sakti Bahari took over foreign shares in PT. Tirtamas Mutiara thus changed the status of the PMA company to become PMDN for the location of the waters of Pangkajarat Bay, Bima Regency, NTB.</p>'
+                ],
+                'en' => [
+                    'title' => 'History',
+                    'content' => '<p>In 1986, the company obtained a pearl cultivation business license from the Director-General of Fisheries No.IK.120/03.5757/86, dated September 29, 1986, and a Permanent Approval Letter of PMDN No.269/I/PMDN/1986 dated November 24, 1986.</p><p>In 1992, the company obtained a Permanent Business License from the Chairman of BKPM No.30/T/PERTANIAN/1992 dated January 25, 1992, wherefrom December 1988, the company was producing commercially for the area in Sape Bay, Bima Regency, NTB.</p><p>In 2020, PT. Bima Sakti Bahari received recommendations for the extension of its business in trade and fisheries business for the waters of Sape bay and Tengge waters and waters of Radu, Bima Regency, NTB Province</p><p>In 2021, PT. Bima Sakti Bahari took over foreign shares in PT. Tirtamas Mutiara thus changed the status of the PMA company to become PMDN for the location of the waters of Pangkajarat Bay, Bima Regency, NTB.</p>'
+                ],
                 'img' => null,
+            ],
+            'visi' => [
+                'id' => 'Participate in making Indonesia a world power in the field of fisheries and marine.',
+                'em' => 'Participate in making Indonesia a world power in the field of fisheries and marine.',
+            ],
+            'misi' => [
+                'id' => [
+                    10 => "Contribute as one of the leading national private companies in the field of fisheries and marine.",
+                    20 => "Improve Indonesia's human resources capability as a driver of the nation's economy.",
+                    30 => "Developing Indonesia's marine resources technology becomes a national pride that is appreciated by the world.",
+                    40 => "Run a sustainable business model about the environmental conditions and the welfare of the company's human resources.",
+                ],
+                'en' => [
+                    10 => "Contribute as one of the leading national private companies in the field of fisheries and marine.",
+                    20 => "Improve Indonesia's human resources capability as a driver of the nation's economy.",
+                    30 => "Developing Indonesia's marine resources technology becomes a national pride that is appreciated by the world.",
+                    40 => "Run a sustainable business model about the environmental conditions and the welfare of the company's human resources.",
+                ]
             ],
         ];
         if (file_exists($file)){ $arrConf = json_decode(file_get_contents($file),true); }
         else{file_put_contents($file, json_encode($arrConf));}
         return view('cms.page.page-config-home', compact( 'arrConf' ));
+    }
+
+    public function store(Request $httpRequest)
+    {
+        $res = ['response' => true];
+        $file = $this->getFileDir;
+        $arrConf = json_decode(file_get_contents($file),true);
+        if ($httpRequest->type == 'string') {
+            $new_arr = $this->storeString($arrConf,$httpRequest->input());
+        }else if ($httpRequest->type == 'img') {
+            $new_arr = $this->storeImg($arrConf,$httpRequest->input());
+        }
+        unlink($file);
+        file_put_contents($file, json_encode($new_arr));
+        return response()->json($res);
+    }
+
+    private function storeString($arrConf,$input)
+    {
+        $arrConf['intruduction']['id']['title'] = $input['intruduction_id_title'];
+        $arrConf['intruduction']['id']['content'] = $input['intruduction_id_content'];
+        $arrConf['intruduction']['en']['title'] = $input['intruduction_en_title'];
+        $arrConf['intruduction']['en']['content'] = $input['intruduction_en_content'];
+        $arrConf['visi']['id'] = $input['visi_id'];
+        $arrConf['visi']['en'] = $input['visi_en'];
+        $arrConf['misi']['id'] = json_deocede($input['misi_id'],true);
+        $arrConf['misi']['en'] = json_deocede($input['misi_en'],true);
+        return $arrConf;
+    }
+
+    private function storeImg($arrConf,$input)
+    {
+        if (isset($arrConf[$input['for']][$input['key']]) and !empty($arrConf[$input['for']][$input['key']])) { unlink($arrConf[$input['for']][$input['key']]); }
+        $dir_estimate = 'config_img';
+        $dir_file = '';
+        foreach (explode('/',$dir_estimate) as $item) {
+            $dir_file .= $item.'/';
+            if (!file_exists($dir_file)){ mkdir($dir_file, 0777); }
+        }
+        $path_file = $dir_file.$input['for'].'_'.$input['key'].'_'.$input['img_name'];
+        try {
+            file_put_contents($path_file, base64_decode($input['img_encode']));
+        } catch (Exception $e) {
+            $msg = $e->getMessage();
+            return response()->json([
+                'response' => false,
+                'http_req' => $msg
+            ]);
+        }
+        $arrConf[$input['for']][$input['key']] = $path_file;
+        return $arrConf;
     }
 }
---
+++ resources/views/cms/page/page-config-about-us.blade.php
@@ -0,0 +1,178 @@
+@extends('cms.layout.base')
+
+@section('title')
+About Us Page Config
+@endsection
+
+@push('link')
+@endpush
+
+@push('content')
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div id="PublicConfig">
+                <form onsubmit="return submitPageConfig()"  enctype="multipart/form-data">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-title">
+                                <h3>Form <b></b></h3>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col form-group">
+                                    <label for="intruduction_id_title">Introduction Title Indonesia</label>
+                                    <input type="text" class="form-control" id="intruduction_id_title" name="intruduction_id_title" value="{{ $arrConf['intruduction']['id']['title'] }}" required>
+                                </div>
+                                <div class="col form-group">
+                                    <label for="intruduction_en_title">Introduction Title English</label>
+                                    <input type="text" class="form-control" id="intruduction_en_title" name="intruduction_en_title" value="{{ $arrConf['intruduction']['en']['title'] }}" required>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col form-group">
+                                    <label for="intruduction_id_content">Introduction Content Indonesia</label>
+                                    <textarea id="intruduction_id_content" name="intruduction_id_content">{{ $arrConf['intruduction']['id']['content'] }}</textarea>
+                                </div>
+                                <div class="col form-group">
+                                    <label for="intruduction_en_content">Introduction Content Indonesia</label>
+                                    <textarea id="intruduction_en_content" name="intruduction_en_content">{{ $arrConf['intruduction']['en']['content'] }}</textarea>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col form-group">
+                                    <label for="intruduction_img">Introduction Img @if(isset($arrConf['intruduction']['img']) and $arrConf['intruduction']['img'] != null and $arrConf['intruduction']['img'] != '')<a href="{{ url($arrConf['intruduction']['img']) }}" target="_blank" rel="noopener noreferrer">show</a>@endif</label>
+                                    <input type="file" class="form-control" id="intruduction_img" name="intruduction_img" accept="image/*">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col form-group">
+                                    <label for="visi_id">Visis Indonesia</label>
+                                    <input type="text" class="form-control" id="visi_id" name="visi_id" value="{{ $arrConf['visi']['id'] }}" required>
+                                </div>
+                                <div class="col form-group">
+                                    <label for="visi_en">Visis English</label>
+                                    <input type="text" class="form-control" id="visi_en" name="visi_en" value="{{ $arrConf['visi']['en'] }}" required>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <div class="card-title">
+                                                Misi Indonesia
+                                            </div>
+                                        </div>
+                                        <div class="card-body">
+                                            @foreach($arrConf['misi']['id'] as $idx => $data)
+                                            <div id="row_misi_id_{{ $idx }}" class="input-group input-group-sm">
+                                                <input type="text" class="form-control misi_id" requred value="{{$data}}">
+                                                <span class="input-group-append">
+                                                    <button onclick="removeMisi('row_misi_id_{{ $idx }}')" type="button" class="btn btn-info btn-flat"><i class="far fa-trash"></i></button>
+                                                </span>
+                                            </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <div class="card-title">
+                                                Misi English
+                                            </div>
+                                        </div>
+                                        <div class="card-body">
+                                            @foreach($arrConf['misi']['en'] as $idx => $data)
+                                            <div id="row_misi_en_{{ $idx }}" class="input-group input-group-sm">
+                                                <input type="text" class="form-control misi_en" requred value="{{$data}}">
+                                                <span class="input-group-append">
+                                                    <button onclick="removeMisi('row_misi_en_{{ $idx }}')" type="button" class="btn btn-info btn-flat"><i class="far fa-trash"></i></button>
+                                                </span>
+                                            </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col">
+                                    <button type="submit" class="btn btn-sm btn-block btn-outline-success">Submit</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endpush
+
+@push('script')
+<script>
+    $( document ).ready(function() {
+        $('[name=intruduction_id_content]').summernote()
+        $('[name=intruduction_en_content]').summernote()
+    });
+
+    removeMisi = (target) => { $('#'+target).remove() }
+
+    submitPageConfig = () => {
+        loadingScreen(true)
+        submitPageConfigExe()
+        return false
+    }
+    submitPageConfigExe = async () =>{
+        const storeString = await submitPageConfigExeSend()
+        if (storeString == true) { await storeImg() }
+    }
+    submitPageConfigExeSend = async () =>{
+        let param = {}
+        param.type = 'string'
+        param.intruduction_id_title = $('[name=intruduction_id_title]').val()
+        param.intruduction_en_title = $('[name=intruduction_en_title]').val()
+        param.intruduction_id_content = $('[name=intruduction_id_content]').summernote('code')
+        param.intruduction_en_content = $('[name=intruduction_en_content]').summernote('code')
+        const resStore = await httpRequest('{{ route("cms.page-config.home.store") }}','post',param).then(function(result){ return result })
+        return true
+    }
+    storeImg = async () => {
+        let pictures = $('[name=intruduction_img]').prop('files')
+        let count_img = pictures.length
+        if (count_img == 0) {
+            showPNotify('Info','Success','info')
+            loadingScreen(false)
+            location.reload()
+        }else{
+            $.each(pictures, async function(idx,img){
+                var reader = new FileReader();
+                reader.readAsArrayBuffer(img);
+                reader.onloadend = async function(oFREvent) {
+                    var byteArray = new Uint8Array(oFREvent.target.result)
+                    var len = byteArray.byteLength
+                    var binary = ''
+                    for (var i = 0; i < len; i++) { binary += String.fromCharCode(byteArray[i]) }
+                    byteArray = window.btoa(binary)
+                    let param =  {
+                        'img_encode':byteArray,
+                        'img_name':img.name,
+                        'img_type':img.type,
+                        'type':'img',
+                        'key':'img',
+                        'for':'intruduction'
+                    }
+                    httpRequest('{{ route("cms.page-config.home.store") }}','post',param).then(function(result){ 
+                        showPNotify('Info','Success','info')
+                        loadingScreen(false)
+                        location.reload()
+                    })
+                };
+            })
+        }
+    }
+</script>
+@endpush
