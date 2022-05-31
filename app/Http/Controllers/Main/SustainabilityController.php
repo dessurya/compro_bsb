c375095328fe2353f6582c6542173588a92ab1df
Adam Surya Des <desadamsury93@gmail.com>
2022-05-31 22:15:26 +0700

    .

--- app/Http/Controllers/Main/SustainabilityController.php
+++ app/Http/Controllers/Main/SustainabilityController.php
@@ -6,14 +6,40 @@ use Illuminate\Support\Facades\App;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Sustainability;
+use App\Models\NavigationConfig;
 
 class SustainabilityController extends Controller
 {
+    protected $dirConfig = 'config_json/sustainability.json';
+
+    private function getConfigJSON($dirFile)
+    {
+        if (file_exists($dirFile)){ return json_decode(file_get_contents($dirFile),true); }
+        else { return false; }
+    }
+
     public function index()
     {
+        $config = $this->getConfigJSON($this->dirConfig);
+        if ($config == false) {
+            return response()->json([
+                'response' => true,
+                'msg' => 'This underconstruction'
+            ]);
+        }
+        $arrConfig = NavigationConfig::whereIn('identity',['Sustainability'])->first();
         $lang = App::getLocale();
-        $title_page = 'Sustainability';
-        $banner = url('pict_content_asset/_default/home_1.jpg');
+        $meta_title = json_decode($arrConfig->meta_title,true);
+        $meta_description = json_decode($arrConfig->meta_description,true);
+        $meta_keywords = json_decode($arrConfig->meta_keywords,true);
+        $meta = [
+            'author' => $arrConfig->meta_author,
+            'title' => $meta_title[$lang],
+            'description' => $meta_description[$lang],
+            'keywords' => $meta_keywords[$lang],
+        ];
+        $title_page = $config['title'][$lang];
+        $banner = url($config['banner']['img']);
         $Sustainability = Sustainability::where([
             'flag_publish' => 'Y',
             'language' => $lang
@@ -22,6 +48,6 @@ class SustainabilityController extends Controller
         ];
         $js = [
         ];
-        return view('main.page.sustainability', compact('lang','css','js','title_page','banner','Sustainability'));
+        return view('main.page.sustainability', compact('lang','css','js','title_page','banner','Sustainability','meta'));
     }
 }
