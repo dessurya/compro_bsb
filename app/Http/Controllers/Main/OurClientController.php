0ebb82469be9bd8ecc2315cd4081605c291cfc84
Adam Surya Des <desadamsury93@gmail.com>
2022-05-31 22:19:27 +0700

    .

--- app/Http/Controllers/Main/OurClientController.php
+++ app/Http/Controllers/Main/OurClientController.php
@@ -5,21 +5,48 @@ namespace App\Http\Controllers\Main;
 use Illuminate\Support\Facades\App;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\NavigationConfig;
 
 class OurClientController extends Controller
 {
+    protected $dirConfig = 'config_json/our-client.json';
+
+    private function getConfigJSON($dirFile)
+    {
+        if (file_exists($dirFile)){ return json_decode(file_get_contents($dirFile),true); }
+        else { return false; }
+    }
+
     public function index()
     {
+        $config = $this->getConfigJSON($this->dirConfig);
+        if ($config == false) {
+            return response()->json([
+                'response' => true,
+                'msg' => 'This underconstruction'
+            ]);
+        }
+        $arrConfig = NavigationConfig::whereIn('identity',['Our Client'])->first();
         $lang = App::getLocale();
-        $title_page = 'Our Client';
+
+        $meta_title = json_decode($arrConfig->meta_title,true);
+        $meta_description = json_decode($arrConfig->meta_description,true);
+        $meta_keywords = json_decode($arrConfig->meta_keywords,true);
+        $meta = [
+            'author' => $arrConfig->meta_author,
+            'title' => $meta_title[$lang],
+            'description' => $meta_description[$lang],
+            'keywords' => $meta_keywords[$lang],
+        ];
+        $title_page = $config['title'][$lang];
         $page_data = [
-            'content' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-            'img' => url('config_img/client.png')
+            'content' => $config['description'][$lang],
+            'img' => url($config['maps']['img'])
         ];
         $css = [
         ];
         $js = [
         ];
-        return view('main.page.our-client', compact('lang','css','js','title_page','page_data'));
+        return view('main.page.our-client', compact('lang','css','js','title_page','page_data','meta'));
     }
 }
