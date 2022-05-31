6bc92f9db527c8f67d1d5a7ca6d2c28c31b13c3b
Adam Surya Des <desadamsury93@gmail.com>
2022-05-31 20:56:01 +0700

    .

--- app/Http/Controllers/Main/HomeController.php
+++ app/Http/Controllers/Main/HomeController.php
@@ -99,9 +99,9 @@ class HomeController extends Controller
         return Redirect::back();
     }
 
-    public static function getWebName(HomeController $selfClass)
+    public static function getWebName()
     {
-        $config = $selfClass->getConfigJSON($selfClass->dirPublicConfig);
+        $config = self::getConfigJSON(self::dirPublicConfig);
         return $config['web']['name'];
     }
 
@@ -143,7 +143,7 @@ class HomeController extends Controller
         echo view('main._struct.header', compact('menu','icon','lang'))->render();
     }
 
-    public static function getFooter(HomeController $selfClass)
+    public static function getFooter()
     {
         $arr = [];
         $crYear = '2022';
@@ -154,7 +154,7 @@ class HomeController extends Controller
             url('pict_content_asset/_default/ig-dark.png'),
         ];
         $arr['find'] = $find;
-        $config = $selfClass->getConfigJSON($selfClass->dirPublicConfig);
+        $config = self::getConfigJSON(self::dirPublicConfig);
         $arr['address'] = $config['address'];
         $arr['email'] = $config['email'];
         $arr['phone'] = $config['phone'];
