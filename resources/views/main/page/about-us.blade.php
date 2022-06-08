d7391924cfc075f8e9bdf118547d3f254c7bccc6
Adam Surya Des <desadamsury93@gmail.com>
2022-06-08 14:05:41 +0700

    .

--- resources/views/main/page/about-us.blade.php
+++ resources/views/main/page/about-us.blade.php
@@ -125,14 +125,14 @@
             color: rgb(19 169 229);
             font-weight: 600;
         }
-        #mom #staff h3{
+        #mom #staff h4{
             color: rgb(19 169 229);
             font-weight: 500;
         }
         #mom #staff img{
             border-radius: 100%;
             width: 200px;
-            height: 190px;
+            height: 180px;
         }
     /* mom */
 
