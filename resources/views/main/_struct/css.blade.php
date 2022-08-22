ab8363b3a868fe0443f64c0f59383f384243016f
dessurya <fourline66@gmail.com>
2022-08-22 13:43:53 +0700

    .

--- resources/views/main/_struct/css.blade.php
+++ resources/views/main/_struct/css.blade.php
@@ -205,36 +205,36 @@
         fill: none;
         /* stroke: rgba(1,160,228,1); */
         stroke: white;
-        stroke-width: 4;
+        stroke-width: 8;
         transition: stroke-dasharray 600ms cubic-bezier(0.4, 0, 0.2, 1),
             stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1);
     }
     .btn-menu-toggle .line1 {
         stroke-dasharray: 60 207;
-        stroke-width: 4;
+        stroke-width: 8;
     }
     .btn-menu-toggle .line2 {
         stroke-dasharray: 60 60;
-        stroke-width: 4;
+        stroke-width: 8;
     }
     .btn-menu-toggle .line3 {
         stroke-dasharray: 60 207;
-        stroke-width: 4;
+        stroke-width: 8;
     }
     .btn-menu-toggle.opened .line1 {
         stroke-dasharray: 90 207;
         stroke-dashoffset: -134;
-        stroke-width: 4;
+        stroke-width: 8;
     }
     .btn-menu-toggle.opened .line2 {
         stroke-dasharray: 1 60;
         stroke-dashoffset: -30;
-        stroke-width: 4;
+        stroke-width: 8;
     }
     .btn-menu-toggle.opened .line3 {
         stroke-dasharray: 90 207;
         stroke-dashoffset: -134;
-        stroke-width: 4;
+        stroke-width: 8;
     }
 
     @media (max-width: 568px){
