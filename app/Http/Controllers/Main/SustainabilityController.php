a44d9520222b598b284beaad7c1eba064ea2e7f1
dessurya <fourline66@gmail.com>
2022-08-23 11:04:08 +0700

    .

--- app/Http/Controllers/Main/SustainabilityController.php
+++ app/Http/Controllers/Main/SustainabilityController.php
@@ -45,6 +45,7 @@ class SustainabilityController extends Controller
             'language' => $lang
         ])->orderBy('position','asc')->get();
         $css = [
+            url('asset\main\css\sustainability.css'),
         ];
         $js = [
         ];
